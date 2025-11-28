1537cde77565e1b2eb7cba5f3159aa3d2d339e69
goomezCode <danielgdpaula@gmail.com>
2025-11-27 21:55:23 -0300

    add repositor

---
+++ src/test/java/org/web/bytestore/ByteStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.web.bytestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
